949ff6203c5d6b40b0c7e1329c3482789b26cd27
Masahiro Furukawa <masahiro.furukawa.private@gmail.com>
2025-04-01 00:02:50 +0900

    Added mbed source

---
+++ projects/whc2025/mbed/main.cpp
@@ -0,0 +1,247 @@
+/**
+ * Masahiro FURUKAWA - m.furukawa@ist.osaka-u.ac.jp
+ *
+ * Dec 26, 2017
+ * Aug 29, 2018 
+ * Dec 17, 2019 @ Digital Low Pass Filter 5Hz -> No LPF
+                @ ACC range 2G -> 4G
+                @ GYRO range 250 -> 500 Degree per second
+                @ Deleted magnet sensor checking 
+                @ Set Acc Data Rates, Enable Acc LPF , Bandwidth 218Hz
+                @ Use DLPF set Gyroscope bandwidth 5Hz, temperature bandwidth 5Hz
+ * Mar 31, 2025 @ Two Channels (Ch2, Ch3), 1kHz for WHC2025
+ *
+ * MPU9250 9DoF Sensor (Extended to Ch1 ~ Ch4)
+ *
+ **/
+
+#include "mbed.h"
+#include "MPU9250.h"
+
+/* MPU9250 Library
+ *
+ *  https://developer.mbed.org/users/kylongmu/code/MPU9250_SPI_Test/file/5839d1b118bc/main.cpp
+ *
+ *   MOSI (Master Out Slave In)  p5
+ *   MISO (Master In Slave Out   p6
+ *   SCK  (Serial Clock)         p7
+ *   ~CS  (Chip Select)          p8 -> p30
+ */
+
+// define serial objects
+Serial          pc(USBTX, USBRX);
+
+Ticker          ticker;
+Timer           timer;
+
+#define nCh     4   // number of ch
+
+float     SampleFreq = 1000.0f;   // [Hz]
+unsigned int counter = 0;
+unsigned int usCycle = 1000000/SampleFreq ;
+
+int errFlag = 0;
+
+//define the mpu9250 object
+mpu9250_spi     *imu[nCh];
+
+// define SPI object for imu objects
+SPI             spi1(p5,  p6,  p7);
+SPI             spi2(p11, p12, p13);
+
+void init(void)
+{
+    pc.baud(921600); //921600 / 115200
+
+    imu[0] = new mpu9250_spi(spi1, p30); // Ch1 on PCB
+    imu[1] = new mpu9250_spi(spi1, p29); // Ch2 on PCB *spi1*
+    imu[2] = new mpu9250_spi(spi2, p23); // Ch3 on PCB *spi2*
+    imu[3] = new mpu9250_spi(spi2, p21); // Ch4 on PCB
+
+    for(int i=0; i<nCh; i++) {
+
+        imu[0]->deselect();
+        imu[1]->deselect(); // Ch2 on PCB *spi1*
+        imu[2]->deselect(); // Ch3 on PCB *spi2*
+        imu[3]->deselect();
+        
+        imu[i]->select();
+
+        //INIT the mpu9250
+        //if(imu[i]->init(1,BITS_DLPF_CFG_188HZ)) 
+        //if(imu[i]->init(1,BITS_DLPF_CFG_5HZ))
+        if(imu[i]->init(1,BITS_DLPF_CFG_256HZ_NOLPF2))
+        { 
+            printf("\nCH %d ----- Couldn't initialize", i+1);
+            wait(10);
+        }
+        //output the I2C address to know if SPI is working, it should be 104
+        printf("\nCH %d ----- WHOAMI = 0x%2x",i+1, imu[i]->whoami());         
+               
+        if(imu[i]->whoami() != 0x71) {
+            printf(" *** ERROR *** Did not respond correctly!");
+            errFlag |= 0x01<<(i*2);
+        }else{
+            printf(" Acc_scale = %u[G] ",   imu[i]->set_acc_scale(BITS_FS_16G));        wait_ms(10);
+            printf(" Gyro_scale = %u[DPS]", imu[i]->set_gyro_scale(BITS_FS_2000DPS));   wait_ms(10);
+            //printf("AK8963 WHIAM=0x%2x\n",imu[i]->AK8963_whoami());
+            
+            //if(imu[i]->AK8963_whoami() != 0x48) {
+            //    printf(" *** ERROR *** magnetrometer does not respond correctly!\n");
+            //    errFlag |= 0x02<<(i*2);
+            //}
+            
+            //imu[i]->AK8963_calib_Magnetometer();
+            //wait_ms(10);
+        }
+    }
+    imu[0]->deselect();     
+    imu[1]->select();                           // Ch2 on PCB *spi1*
+    imu[2]->select();                           // Ch3 on PCB *spi2*
+    imu[3]->deselect();
+}
+
+void eventFunc(void)
+{
+    // limitation on sending bytes at 921600bps - 92bits(under 100us/sample)
+    // requirement : 1kHz sampling
+    // = 921.5 bits/sample
+    // = 115.1 bytes/sample
+    // = 50 bytes/axis (2byte/axis)
+    
+    // 2 byte * 6 axes * 2 ch = 24 bytes/sample
+
+    // imu[0]->select();       imu[1]->deselect();
+    // imu[2]->select();       imu[3]->deselect();
+    
+    // imu[0]->read_acc();    imu[0]->read_rot();            
+    // imu[2]->read_acc();    imu[2]->read_rot();
+    
+    // imu[0]->deselect();    imu[1]->select();
+    // imu[2]->deselect();    imu[3]->select();
+    
+    // imu[1]->read_acc();    imu[1]->read_rot();    
+    // imu[3]->read_acc();    imu[3]->read_rot();
+    
+    imu[1]->read_acc();    imu[1]->read_rot();  // Ch2 on PCB *spi1*
+    imu[2]->read_acc();    imu[2]->read_rot();  // Ch3 on PCB *spi2*
+
+    // set header 
+    putc(127, stdout); // DEL in ascii 
+
+    // Ch2(imu[1]) on PCB *spi1*
+    // Ch3(imu[2]) on PCB *spi2*
+    for(int i=1; i<=2; i++) {
+        // 2 byte * 6 axes * 2 ch = 24 bytes/sample
+        for(int j=0; j<6; j++) putc(imu[i]->accelerometer_response[j], stdout);
+        for(int j=0; j<6; j++) putc(imu[i]->gyroscope_response[j],     stdout);
+    }
+        
+    // send time stamp // 4バイト（ビッグエンディアン）で送信
+    //putc((counter >> 24) & 0xFF, stdout); // 最上位バイト
+    putc((counter >> 16) & 0xFF, stdout); // 2番目のバイト
+    putc((counter >> 8) & 0xFF, stdout);  // 3番目のバイト
+    putc(counter & 0xFF, stdout);         // 最下位バイト
+
+    counter++;
+}
+
+void set_acc_scale_all(int val){ 
+    for(int i=0; i<nCh; i++) {
+        imu[0]->deselect();
+        imu[1]->deselect();
+        imu[2]->deselect();
+        imu[3]->deselect();
+    
+        imu[i]->select();
+        imu[i]->set_acc_scale(val);
+        wait_ms(2);
+    }
+}
+void set_gyro_scale_all(int val){ 
+    for(int i=0; i<nCh; i++) {
+        imu[0]->deselect();
+        imu[1]->deselect();
+        imu[2]->deselect();
+        imu[3]->deselect();
+    
+        imu[i]->select();
+        imu[i]->set_gyro_scale(val);
+        wait_ms(2);
+    }
+}
+void set_LPF_all(int val){ 
+    for(int i=0; i<nCh; i++) {
+        imu[0]->deselect();
+        imu[1]->deselect();
+        imu[2]->deselect();
+        imu[3]->deselect();
+    
+        imu[i]->select();
+        imu[i]->init(1, val);  
+        wait_ms(2);
+    }
+}
+
+int main()
+{
+    // make instances and check sensors
+    init();    
+
+    char c;
+
+    while(1) {
+        if(pc.readable()) {
+            c = pc.getc();
+
+            switch(c){
+                
+                // reset
+                case 'r':
+                    ticker.detach();
+                    break;
+                
+                // start sampling
+                case 's': 
+                    counter = 0;
+                    ticker.attach_us(eventFunc, 1000000.0f/SampleFreq);
+                    break;
+
+                // set ACC range
+                case 'A':
+                    ticker.detach();        
+                    c = pc.getc();  // read second byte
+                    if('0' == c)  set_acc_scale_all(BITS_FS_2G);  
+                    if('1' == c)  set_acc_scale_all(BITS_FS_4G);
+                    if('2' == c)  set_acc_scale_all(BITS_FS_8G);
+                    if('3' == c)  set_acc_scale_all(BITS_FS_16G); 
+                    break;
+
+                // set GYRO range
+                case 'G':
+                    ticker.detach();        
+                    c = pc.getc();  // read second byte
+                    if('0' == c)  set_gyro_scale_all(BITS_FS_250DPS);  
+                    if('1' == c)  set_gyro_scale_all(BITS_FS_500DPS);
+                    if('2' == c)  set_gyro_scale_all(BITS_FS_1000DPS);
+                    if('3' == c)  set_gyro_scale_all(BITS_FS_2000DPS); 
+                    break;
+
+                // set LPF cutoff frequecy
+                case 'L':
+                    ticker.detach();        
+                    c = pc.getc();  // read second byte
+                    if('0' == c)  set_LPF_all(BITS_DLPF_CFG_256HZ_NOLPF2);  
+                    if('1' == c)  set_LPF_all(BITS_DLPF_CFG_188HZ);
+                    if('2' == c)  set_LPF_all(BITS_DLPF_CFG_98HZ);
+                    if('3' == c)  set_LPF_all(BITS_DLPF_CFG_42HZ); 
+                    if('4' == c)  set_LPF_all(BITS_DLPF_CFG_20HZ);  
+                    if('5' == c)  set_LPF_all(BITS_DLPF_CFG_10HZ);
+                    if('6' == c)  set_LPF_all(BITS_DLPF_CFG_5HZ);
+                    if('7' == c)  set_LPF_all(BITS_DLPF_CFG_2100HZ_NOLPF); 
+                    break;
+
+            }
+        }
+    }
+}
---
+++ projects/whc2025/read_acc_3_0_1_forWHC(2Ch).py
@@ -0,0 +1,235 @@
+# from https://github.com/mfurukawa/imu_sensor/tree/master/src/Python 
+# March 31, 2025 by Masahiro Furukawa, furukawa.masahiro.ist@osaka-u.ac.jp
+# September 03, 2020 by Yuto Nakayachi 
+
+
+from __future__ import unicode_literals ,print_function
+import serial 
+from time import sleep 
+import numpy as np 
+import matplotlib.pyplot as plt
+import io 
+import csv
+import time 
+import datetime 
+import struct 
+
+
+# Variable to get real value 
+MPU9250A_2g  =     0.000061035156 # 0.000061035156 g/LSB
+MPU9250A_4g  =     0.000122070312 # 0.000122070312 g/LSB
+MPU9250A_8g  =     0.000244140625 # 0.000244140625 g/LSB
+MPU9250A_16g =     0.000488281250 # 0.000488281250 g/LSB
+
+MPU9250G_250dps  = 0.007633587786 # 0.007633587786 dps/LSB
+MPU9250G_500dps  = 0.015267175572 # 0.015267175572 dps/LSB
+MPU9250G_1000dps = 0.030487804878 # 0.030487804878 dps/LSB
+MPU9250G_2000dps = 0.060975609756 # 0.060975609756 dps/LSB
+
+MPU9250M_4800uT  = 0.6            # 0.6 uT/LSB
+
+MPU9250T_85degC  = 0.002995177763 # 0.002995177763 degC/LSB
+Magnetometer_Sensitivity_Scale_Factor = 0.15
+
+# number of axis 
+numVariable = 24 # 4ch * 6acc 
+
+# Maximum time for measure
+minuteLength = 25 
+
+# sampling rate 
+smplHz = 500 
+
+# Variable to count number of sampling 
+smpl_cnt = 0
+
+# Variable to count number of fail
+fail_cnt_byte = 0
+fail_cnt_head = 0
+
+# Array to store data
+buf = [[0 for i in range(numVariable + 2)] for j in range(smplHz*60*minuteLength)] 
+
+# Array to store real value 
+buf_f = [[0 for i in range(numVariable + 2)] for j in range(smplHz*60*minuteLength)]
+
+# define serial port 
+ser = serial.Serial("COM3",921600,timeout=1) 
+
+
+# Check serial connection 
+if ser.is_open:
+    print("Start Serial Connection") 
+else:
+    print("PORT ERROR") 
+    ser.close() 
+    exit() 
+
+# Function to create csv file 
+def writeCSV():
+    global ser 
+    global smpl_cnt 
+    global buf 
+    global buf_f 
+    ser.write(b'r') 
+    print("Start Create CSV File") 
+    head = ["sample_cc","ms","ACC_X1","ACC_Y1","ACC_Z1","GYRO_X1","GYRO_Y1","GYRO_Z1","ACC_X2","ACC_Y2","ACC_Z2","GYRO_X2","GYRO_Y2","GYRO_Z2","ACC_X3","ACC_Y3","ACC_Z3","GYRO_X3","GYRO_Y3","GYRO_Z3","ACC_X4","ACC_Y4","ACC_Z4","GYRO_X4","GYRO_Y4","GYRO_Z4"]
+    dt_now = datetime.datetime.now() 
+    year = dt_now.year 
+    month = dt_now.month 
+    day = dt_now.day 
+    hour = dt_now.hour 
+    minute = dt_now.minute 
+    t = str(year)+str(month)+str(day)+str(hour)+str(minute)
+    title_int = "acc_data"+str(t)+"_int"+".csv"
+    FILE_int = open(title_int,"w",newline="") 
+    title_float="acc_data"+str(t)+"_float"+".csv" 
+    FILE_float = open(title_float,"w",newline="")    
+    wi = csv.writer(FILE_int)
+    wi.writerow(head)
+    wf = csv.writer(FILE_float) 
+    wf.writerow(head) 
+    for i in range(smpl_cnt):
+        wi.writerow(buf[i]) 
+        wf.writerow(buf_f[i]) 
+    FILE_int.close() 
+    FILE_float.close() 
+    print()
+    print(title_int+" "+"created") 
+    print(title_float+" "+"created")
+    print()
+    print("Done Create CSV File") 
+       
+# Function to Measure 
+def readByte():
+    global ser 
+    global smpl_cnt 
+    global buf 
+    global buf_f  
+    global xl 
+    global yl 
+    global fail_cnt_byte 
+    global fail_cnt_head 
+
+    ser.write(b"r") 
+    time.sleep(0.01)
+    ser.write(b"s") 
+    time.sleep(0.01) 
+
+    state = 0
+    store = []
+    while(1):
+        res = ser.read()
+
+        if state == 0 and res == b'\r':
+            res=ser.read() 
+            if res == b'\n':
+                state = 1 
+                store = []
+            else:
+                #print("End byte set error")
+                fail_cnt_byte += 1 
+                #time.sleep(2)
+        
+        elif state == 1:
+            store.append(res) 
+                
+            if len(store)==50:
+
+                # check header 
+                if store[0]==b'*':
+                    del store[0]
+                else:
+                    #print("header error") 
+                    #time.sleep(2)
+                    fail_cnt_head += 1 
+                    state = 0
+                    store = []
+                    continue 
+
+        
+                #add time stamp
+                if smpl_cnt==0:
+                    #start_time = int.from_bytes(res[-1],"big")
+                    start_time = struct.unpack("b",store[-1])[0]
+                    #start_time = store[-1]
+                    #print(start_time)
+                    tmp_time = 0
+                else:
+                    #now_time = int.from_bytes(res[-1],"big") 
+                    add_time = struct.unpack("b",store[-1])[0]
+                    #add_time = store[-1]
+                    tmp_time += add_time
+                buf[smpl_cnt][1] = tmp_time
+                buf_f[smpl_cnt][1] = tmp_time
+                buf[smpl_cnt][0] = smpl_cnt
+                buf_f[smpl_cnt][0] = smpl_cnt
+
+                # store data 
+                for i in range(0,48,2):
+                    res2 = store[i:i+2] 
+                    tup = struct.unpack('>h', b''.join(res2)) 
+                    val = tup[0]
+                    num = i%12 
+                    ch = i//12 
+                    buf[smpl_cnt][6*ch + num//2 + 2]=val 
+                    if (num//2)>=3:
+                        buf_f[smpl_cnt][6*ch + num//2 + 2]=val * MPU9250G_500dps
+                    else:
+                        buf_f[smpl_cnt][6*ch + num//2 + 2]=val * MPU9250A_4g
+
+                """           
+                print (
+                    "{:+.3f}".format(buf_f[smpl_cnt][2]) ,      "{:+.3f}".format(buf_f[smpl_cnt][3]) ,      "{:+.3f}".format(buf_f[smpl_cnt][4]) , 
+                    "{:+.3f}".format(buf_f[smpl_cnt][5]) ,  "{:+.3f}".format(buf_f[smpl_cnt][6]) ,  "{:+.3f}".format(buf_f[smpl_cnt][7]) ,'\t',
+                    
+                    "{:+.3f}".format(buf_f[smpl_cnt][8]) ,      "{:+.3f}".format(buf_f[smpl_cnt][9]) ,      "{:+.3f}".format(buf_f[smpl_cnt][10]) , 
+                    "{:+.3f}".format(buf_f[smpl_cnt][11]) ,  "{:+.3f}".format(buf_f[smpl_cnt][12]) ,  "{:+.3f}".format(buf_f[smpl_cnt][13]) , '\t',
+                    
+                    "{:+.3f}".format(buf_f[smpl_cnt][14]) ,      "{:+.3f}".format(buf_f[smpl_cnt][15]) ,      "{:+.3f}".format(buf_f[smpl_cnt][16]) , 
+                    "{:+.3f}".format(buf_f[smpl_cnt][17]) ,  "{:+.3f}".format(buf_f[smpl_cnt][18]) ,  "{:+.3f}".format(buf_f[smpl_cnt][19]) , '\t',
+                    
+                    "{:+.3f}".format(buf_f[smpl_cnt][20]) ,      "{:+.3f}".format(buf_f[smpl_cnt][21]) ,      "{:+.3f}".format(buf_f[smpl_cnt][22]) , 
+                    "{:+.3f}".format(buf_f[smpl_cnt][23]) ,  "{:+.3f}".format(buf_f[smpl_cnt][24]) ,  "{:+.3f}".format(buf_f[smpl_cnt][25]) ) 
+                """
+                
+                smpl_cnt += 1
+                store = []
+                state = 0
+
+            if smpl_cnt>=10000:
+                break 
+
+# Start
+print("ready? --> press s key") 
+while(1):
+    ready_s = input() 
+    if ready_s == "s":
+        break 
+    if ready_s == "r":
+        print("over") 
+        ser.close()
+        exit() 
+
+# Measure the start time 
+p_time = time.time() 
+
+# Function to measure 
+readByte()
+
+# Measure the end time 
+e_time = time.time() 
+
+# The time it took 
+print("time: ",e_time - p_time) 
+
+# Function to create csv file 
+writeCSV() 
+
+# close serial port 
+ser.close() 
+
+print("number of data: ",smpl_cnt) 
+print("number of byte fail: ",fail_cnt_byte)
+print("number of header fail: ",fail_cnt_head)
+print("END")
